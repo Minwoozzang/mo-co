09421b074f98735d80b7ae8674886d0aeb53f42b
dada992 <olive_js_933@naver.com>
2023-02-20 10:49:39 +0900

    [feat] 주소 버튼 수정

--- src/pages/teampage/TeamPlace.jsx
+++ src/pages/teampage/TeamPlace.jsx
@@ -6,6 +6,7 @@ import { db } from '../../common/firebase';
 import { doc, updateDoc, getDoc } from 'firebase/firestore';
 import styled from '@emotion/styled';
 import TeamPlaceModal from '../../components/teamPage/TeamPlaceModal';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 export default function TeamPlace() {
   const { id } = useParams();
@@ -13,6 +14,10 @@ export default function TeamPlace() {
   const [convert, setConvert] = useState(false);
   const [currentUserId, setCurrentUserId] = useState('');
   const [idUid, setidUid] = useState([]);
+  const [address, setAddress] = useState('주소를 등록해주세요!'); // 주소
+  const [placeName, setPlaceName] = useState('');
+  const [placeX, setPlaceX] = useState(33.450701);
+  const [placeY, setPlaceY] = useState(126.570667);
 
   const postDoc = doc(db, 'post', id);
 
@@ -39,7 +44,8 @@ export default function TeamPlace() {
   const updateContentPlace = async () => {
     if (convert) {
       const newContentField = {
-        contentPlace: addressDetail,
+        contentPlaceName: address,
+        contentPlaceAddress: placeName,
       };
       try {
         await updateDoc(postDoc, newContentField);
@@ -102,26 +108,80 @@ export default function TeamPlace() {
         {convert ? (
           <>
             <ContentCard>
-              <PlaceBtn
-                onClick={() => {
-                  setModal(!modal);
-                }}
-              >
-                주소를 등록해주세요!
-              </PlaceBtn>
+              <PlaceWrap>
+                <Map // 지도를 표시할 Container
+                  center={{
+                    // 지도의 중심좌표
+                    lat: placeX,
+                    lng: placeY,
+                  }}
+                  style={{
+                    // 지도의 크기
+                    width: '50%',
+                    height: '15vh',
+                  }}
+                  level={4} // 지도의 확대 레벨
+                >
+                  <MapMarker // 마커를 생성합니다
+                    position={{
+                      // 마커가 표시될 위치입니다
+                      lat: placeX,
+                      lng: placeY,
+                    }}
+                  />
+                </Map>
+                <PlaceTextWrap>
+                  <PlaceBtn
+                    onClick={() => {
+                      setModal(!modal);
+                    }}
+                  >
+                    {address}
+                  </PlaceBtn>
+                  <div>{placeName}</div>
+                </PlaceTextWrap>
+              </PlaceWrap>
               {modal == true ? (
                 <TeamPlaceModal
                   modal={modal}
                   onClose={() => {
                     setModal(false);
                   }}
+                  setAddress={setAddress}
+                  setPlaceName={setPlaceName}
                 />
               ) : null}
             </ContentCard>
           </>
         ) : (
           <ContentCard>
-            <p>{place}</p>
+            <PlaceWrap>
+              <Map // 지도를 표시할 Container
+                center={{
+                  // 지도의 중심좌표
+                  lat: placeX,
+                  lng: placeY,
+                }}
+                style={{
+                  // 지도의 크기
+                  width: '50%',
+                  height: '15vh',
+                }}
+                level={4} // 지도의 확대 레벨
+              >
+                <MapMarker // 마커를 생성합니다
+                  position={{
+                    // 마커가 표시될 위치입니다
+                    lat: placeX,
+                    lng: placeY,
+                  }}
+                />
+              </Map>
+              <PlaceTextWrap>
+                <PlaceBtn>{address}</PlaceBtn>
+                <div>{placeName}</div>
+              </PlaceTextWrap>
+            </PlaceWrap>
           </ContentCard>
         )}
       </TextAreaWrapper>
@@ -174,19 +234,29 @@ const ContentCard = styled.div`
   transition: 0.4s;
   height: 20vh;
   background-color: whitesmoke;
-  button,
-  p {
-    padding: 20px;
-  }
+  padding: 20px;
 `;
 
-const PlaceBtn = styled.button`
-  width: 170px;
-  height: 30px;
-  text-align: center;
+const PlaceBtn = styled.div`
+  width: 100%;
+  height: 10px;
   align-items: center;
   font-size: 15px;
   border: none;
   background-color: transparent;
   color: grey;
+`;
+
+const PlaceWrap = styled.div`
+  display: flex;
+`;
+
+const PlaceTextWrap = styled.div`
+  width: 150px;
+  margin-left: auto;
+  display: flex;
+  flex-direction: column;
+  div {
+    padding: 20px 10px;
+  }
 `;
