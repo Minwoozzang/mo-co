58a4cb275f7ebc49235f3ecc2d0efc46850d46f9
2sel <yunwel0610@gmail.com>
2023-02-24 01:31:30 +0900

    feat. detailrecruit css 파일추가

--- src/components/mateDetail/detailRecruit/DetailRecruit.jsx
+++ src/components/mateDetail/detailRecruit/DetailRecruit.jsx
@@ -8,10 +8,32 @@ import {
   where,
 } from 'firebase/firestore';
 import React, { useState, useEffect } from 'react';
-import styled from '@emotion/styled';
 import { db, authService } from './../../../common/firebase';
 import { useParams } from 'react-router-dom';
 import { Modal } from 'antd';
+import {
+  RecruitWrap,
+  RecruitFont,
+  RecruitArea,
+  AreaDetail,
+  RecruitDate,
+  DateDetail,
+  RecruitStack,
+  StackDetail,
+  RecruitCurrent,
+  RecruitDetail,
+  RecruitBtn,
+  RecruitModal,
+  RecruitModalTitle,
+  RecruitModalContentBox,
+  RecruitModalContent,
+  RecruitModalBtnBox,
+  RecruitModalBtnNo,
+  RecruitModalBtnYes,
+  RecruitFooter,
+  RecruitGuide,
+  UserHr,
+} from './DetailRecruitStyle';
 
 const DetailRecruit = () => {
   const { id } = useParams();
@@ -147,6 +169,7 @@ const DetailRecruit = () => {
         <RecruitFont>모임지역</RecruitFont>
         <AreaDetail>{post.partyLocation}</AreaDetail>
       </RecruitArea>
+      {/* <UserHr /> */}
       <RecruitDate>
         <RecruitFont>모임시간</RecruitFont>
         <DateDetail>{post.partyTime}</DateDetail>
@@ -195,125 +218,3 @@ const DetailRecruit = () => {
 };
 
 export default DetailRecruit;
-
-const RecruitWrap = styled.div`
-  width: 280px;
-  height: 426px;
-  border: 1px solid #d9d9d9;
-  background-color: #232323;
-  padding: 30px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  position: fixed;
-  top: 150px;
-  right: 50px;
-  border-radius: 20px;
-`;
-const RecruitFont = styled.p`
-  font-size: 12px;
-  font-weight: 400;
-  color: #fff;
-`;
-const RecruitArea = styled.div`
-  width: 100%;
-`;
-const AreaDetail = styled.p`
-  font-size: 16px;
-  font-weight: 500;
-  color: #fff;
-`;
-const RecruitDate = styled.div`
-  width: 100%;
-`;
-const DateDetail = styled.p`
-  font-size: 16px;
-  color: #fff;
-`;
-const RecruitStack = styled.div`
-  width: 100%;
-`;
-const StackDetail = styled.p`
-  color: #fff;
-`;
-const RecruitCurrent = styled.div`
-  width: 100%;
-`;
-const RecruitDetail = styled.p`
-  font-size: 16px;
-  color: #fff;
-`;
-const RecruitBtn = styled.button`
-  width: 240px;
-  height: 50px;
-  font-weight: 600;
-  font-size: 16px;
-  border: 1px solid;
-  border-radius: 10px;
-  background-color: #feff80;
-`;
-
-const RecruitModal = styled.form`
-  height: 480px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
-
-const RecruitModalTitle = styled.div`
-  font-size: 22px;
-  font-weight: 500;
-  text-align: center;
-  margin-bottom: 20px;
-`;
-
-const RecruitModalContentBox = styled.div`
-  padding: 20px;
-  flex-direction: row;
-  display: flex;
-`;
-const RecruitModalContent = styled.textarea`
-  flex: 1;
-  padding: 20px;
-  border: 1px solid #d9d9d9;
-  overflow-y: scroll;
-  ::-webkit-scrollbar {
-    display: none;
-  }
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-  height: 100px;
-  outline: none;
-  font-size: 15px;
-`;
-
-const RecruitModalBtnBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  padding: 20px;
-`;
-
-const RecruitModalBtnNo = styled.button`
-  width: 200px;
-  height: 45px;
-  border: 1px solid black;
-  background-color: transparent;
-`;
-
-const RecruitModalBtnYes = styled.button`
-  width: 200px;
-  height: 45px;
-  border: 1px solid black;
-  background-color: transparent;
-`;
-
-const RecruitFooter = styled.div`
-  padding: 20px;
-  color: #b9b9b9;
-`;
-
-const RecruitGuide = styled.div`
-  padding: 20px;
-`;
---
+++ src/components/mateDetail/detailRecruit/DetailRecruitStyle.jsx
@@ -0,0 +1,130 @@
+import styled from '@emotion/styled';
+
+export const RecruitWrap = styled.div`
+  width: 320px;
+  height: 540px;
+  border: 1px solid #d9d9d9;
+  background-color: #232323;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  position: fixed;
+  top: 150px;
+  right: 50px;
+  border-radius: 20px;
+`;
+export const RecruitFont = styled.p`
+  font-size: 12px;
+  font-weight: 400;
+  color: #fff;
+`;
+export const RecruitArea = styled.div`
+  width: 100%;
+`;
+export const AreaDetail = styled.p`
+  font-size: 16px;
+  font-weight: 500;
+  color: #fff;
+`;
+export const RecruitDate = styled.div`
+  width: 100%;
+`;
+export const DateDetail = styled.p`
+  font-size: 16px;
+  color: #fff;
+`;
+export const RecruitStack = styled.div`
+  width: 100%;
+`;
+export const StackDetail = styled.p`
+  color: #fff;
+`;
+export const RecruitCurrent = styled.div`
+  width: 100%;
+`;
+export const RecruitDetail = styled.p`
+  font-size: 16px;
+  color: #fff;
+`;
+export const RecruitBtn = styled.button`
+  width: 240px;
+  height: 50px;
+  font-weight: 600;
+  font-size: 16px;
+  border: 1px solid;
+  border-radius: 10px;
+  background-color: #feff80;
+`;
+
+export const RecruitModal = styled.form`
+  height: 480px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const RecruitModalTitle = styled.div`
+  font-size: 22px;
+  font-weight: 500;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+export const RecruitModalContentBox = styled.div`
+  padding: 20px;
+  flex-direction: row;
+  display: flex;
+`;
+export const RecruitModalContent = styled.textarea`
+  flex: 1;
+  padding: 20px;
+  border: 1px solid #d9d9d9;
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  height: 100px;
+  outline: none;
+  font-size: 15px;
+`;
+
+export const RecruitModalBtnBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+export const RecruitModalBtnNo = styled.button`
+  width: 200px;
+  height: 45px;
+  border: 1px solid black;
+  background-color: transparent;
+`;
+
+export const RecruitModalBtnYes = styled.button`
+  width: 200px;
+  height: 45px;
+  border: 1px solid black;
+  background-color: transparent;
+`;
+
+export const RecruitFooter = styled.div`
+  padding: 20px;
+  color: #b9b9b9;
+`;
+
+export const RecruitGuide = styled.div`
+  padding: 20px;
+`;
+
+export const UserHr = styled.hr`
+  margin-top: 24px;
+  border: 0;
+  height: 0;
+  border-top: 1px solid #8c8c8c;
+`;
