497c841e4fe0c34f052423ae41cf0105998c7850
2sel <yunwel0610@gmail.com>
2023-02-27 10:30:54 +0900

    fix. 버튼 ui 수정

--- src/components/homestyle/homeallbutton.js
+++ src/components/homestyle/homeallbutton.js
@@ -1,24 +1,24 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
 /* HomeAllBtn */
 export const AllBtnContainer = styled.div`
-    width: 100%;
-    height: 503px;
-    /* background-color: skyblue; */
-    padding: 200px 924px 303px;
-`
+  width: 100%;
+  height: 503px;
+  /* background-color: skyblue; */
+  padding: 200px 924px 303px;
+`;
 export const AllBtnTitle = styled.div`
-    width: 72px;
-    height: 20px;
-    font-size: 16px;
-    font-weight: 600;
-    border-radius: 5px;
-    cursor: pointer;
-    color: #E7E7E7;
-    /* margin-top: 120px; */
-    /* background-color: pink; */
-    &:hover {
-    background-color: #D9D9D9;
-    }
-    /* background-color: #f2f2f2; */
-`
+  width: 72px;
+  height: 20px;
+  font-size: 16px;
+  font-weight: 600;
+  border-radius: 5px;
+  cursor: pointer;
+  color: #e7e7e7;
+  /* margin-top: 120px; */
+  /* background-color: pink; */
+  &:hover {
+    color: #feff80;
+  }
+  /* background-color: #f2f2f2; */
+`;
