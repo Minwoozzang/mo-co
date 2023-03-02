3873b8c4633239cd22aed7240559c2396cecc0e9
coachkim <kimsw0825@gmail.com>
2023-03-02 16:28:29 +0900

    pull받기 전 커밋

--- src/common/firebase.js
+++ src/common/firebase.js
@@ -3,6 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import 'firebase/database';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging'
 
 const firebaseConfig = {
   // apiKey: 'AIzaSyCzN2tCMYsWXAox6D_olgaMVQM_DD5X0Tc',
@@ -17,7 +18,7 @@ const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: 'mo-co-92f26',
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
@@ -27,3 +28,31 @@ export const app = initializeApp(firebaseConfig);
 export const authService = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+const messaging = getMessaging(app);
+
+async function requestPermission() {
+  console.log('권한 요청 중...');
+
+  const permission = await Notification.requestPermission();
+  if (permission === "denied") {
+    console.log('알림 권한 허용 안됨');
+    return;
+  }
+
+  console.log("알림 권한이 허용됨");
+
+  const token = await getToken(messaging, {
+    vapidKey: process.env.REACT_APP_FIREBASE_VAPID_KEY,
+  });
+
+  if (token) console.log("token: ", token);
+  else console.log("Can not get Token");
+
+  onMessage(messaging, (payload) => {
+    console.log("메시지가 도착했습니다.", payload);
+    // ...
+  });
+}
+
+requestPermission();
