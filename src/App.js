82a4e9dfc27ade8ed8a269b50ee056b469773f62
wallofwolf <77008335+wallofwolf@users.noreply.github.com>
2023-03-06 22:17:30 +0900

    chore : postState 중복 삭제

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import { useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { authService } from './common/firebase';
 import GlobalStyle from './components/GlobalStyle';
-import usePosts from './hooks/usePost';
 import authState from './recoil/authState';
-import postState from './recoil/postState';
 import Router from './shared/router';
 
 function App() {
   const setAuthState = useSetRecoilState(authState);
-  // post 컬렉션
-  const setPostState = useSetRecoilState(postState);
-  const { data } = usePosts();
 
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
@@ -25,7 +20,6 @@ function App() {
         return;
       }
     });
-    setPostState(data);
   }, [authService.currentUser]);
 
   return (
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,15 +1,10 @@
 import { useRecoilValue } from 'recoil';
 import useUserDoc from '../hooks/useUserDoc';
 import authState from '../recoil/authState';
-import postState from '../recoil/postState';
 
 function Test() {
-  let b = 0;
   const user = useRecoilValue(authState);
   const userDoc = useUserDoc();
-  // postState에 있는 전역 상태를 postCollection에 저장
-  const postCollection = useRecoilValue(postState);
-  console.log('🚀 ~ file: Test.jsx:10 ~ Test ~ a:', postCollection);
 
   return (
     <div>
@@ -22,16 +17,6 @@ function Test() {
       <br />
       <h1>post 컬렉션 TEST</h1>
       <br />
-      {postCollection?.map((item) => (
-        <div key={item.id}>
-          <p>번호 : {(b = b + 1)}</p>
-          <p>id : {item.id}</p>
-          <p>제목 : {item.partyPostTitile}</p>
-          <p>작성자 : {item.nickName}</p>
-          <p>모임명 : {item.partyName}</p>
-          <br />
-        </div>
-      ))}
     </div>
   );
 }
