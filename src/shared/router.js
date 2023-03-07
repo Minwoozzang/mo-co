61ed58364a896eededf0f3817d0708dbe8dd85ce
wallofwolf <77008335+wallofwolf@users.noreply.github.com>
2023-03-07 17:42:26 +0900

    chore : 불필요한 파일 및 라이브러리 제거

--- src/shared/router.js
+++ src/shared/router.js
@@ -11,14 +11,11 @@ import MyPage from '../pages/mypage/MyPage';
 import OnboardingPage from '../pages/onboarding/OnboardingPage';
 import Search from '../pages/search/Search';
 import TeamList from '../pages/teampage/TeamList';
-
 import TeamPage from '../pages/teampage/TeamPage';
 import Home from './../pages/home/Home';
 import MateDetail from './../pages/mate/MateDetail';
 // * 테스트 페이지
 import Test from '../pages/Test';
-import Amplitude from './../amplitude';
-import { authService } from '../common/firebase';
 import { useRecoilValue } from 'recoil';
 import ScrollTop from '../common/scrollTop';
 import authState from '../recoil/authState';
@@ -45,7 +42,6 @@ const Router = () => {
         <Route path="/teamlist/:nickname" element={<TeamList />} />
         {/* 테스트페이지 */}
         <Route path="/test" element={<Test />} />
-        <Route path="/amplitude" element={<Amplitude />} />
       </Routes>
       {user?.uid === null ? null : <MocoChat />}
     </BrowserRouter>
