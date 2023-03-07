37d0fb951bc7ee7b3d204ba839a958df2d3b25dc
2sel <yunwel0610@gmail.com>
2023-03-07 19:24:43 +0900

    fis. 안쓰는 리코일 영역 정리

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import { authService } from './common/firebase';
 import GlobalStyle from './components/GlobalStyle';
 import useTeamPage from './hooks/useTeamPage';
 import authState from './recoil/authState';
-import commentState from './recoil/commentState';
 import Router from './shared/router';
-import useComment from './hooks/useComment';
 import teamPageState from './recoil/teamPageState';
 import postState from './recoil/postState';
 import usePosts from './hooks/usePost';
@@ -16,11 +14,9 @@ import usePosts from './hooks/usePost';
 function App() {
   const setAuthState = useSetRecoilState(authState);
   const setPostState = useSetRecoilState(postState);
-  const setCommentState = useSetRecoilState(commentState);
   const setTeamPageState = useSetRecoilState(teamPageState);
   const post = usePosts();
   const teamPage = useTeamPage();
-  const comment = useComment();
 
   const setUserState = useSetRecoilState(userState);
   const user = useUser();
@@ -38,7 +34,7 @@ function App() {
       }
     });
     setUserState(user.data);
-    setCommentState(comment.data);
+
     setPostState(post.data);
     setTeamPageState(teamPage.data);
   }, [post.data, teamPage.data, user.data]);
--- src/hooks/useComment.jsx
+++ src/hooks/useComment.jsx
@@ -1,23 +1,23 @@
-import { useQuery } from 'react-query';
-import { collection, getDocs, orderBy, query, where } from 'firebase/firestore';
-import { authService, db } from '../common/firebase';
+// import { useQuery } from 'react-query';
+// import { collection, getDocs, orderBy, query, where } from 'firebase/firestore';
+// import { authService, db } from '../common/firebase';
 
-export default function useComment() {
-  return useQuery(
-    'comment',
-    async () => {
-      const q = query(collection(db, 'comment'), orderBy('createdAt', 'desc'));
-      const querySnapshot = await getDocs(q);
-      const comment = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      return comment;
-    },
+// export default function useComment() {
+//   return useQuery(
+//     'comment',
+//     async () => {
+//       const q = query(collection(db, 'comment'), orderBy('createdAt', 'desc'));
+//       const querySnapshot = await getDocs(q);
+//       const comment = querySnapshot.docs.map((doc) => ({
+//         id: doc.id,
+//         ...doc.data(),
+//       }));
+//       return comment;
+//     },
 
-    {
-      cacheTime: 5 * 60 * 1000,
-      staleTime: 2 * 60 * 1000,
-    },
-  );
-}
+//     {
+//       cacheTime: 5 * 60 * 1000,
+//       staleTime: 2 * 60 * 1000,
+//     },
+//   );
+// }
