64bd29cc6351c304e17e4760ab4b427e506110fc
wallofwolf <77008335+wallofwolf@users.noreply.github.com>
2023-03-07 09:41:15 +0900

    refactor : useUserQuery

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,10 +1,20 @@
 import { useRecoilValue } from 'recoil';
 import useUserDoc from '../hooks/useUserDoc';
 import authState from '../recoil/authState';
+import useUserQuery from '../hooks/useUserQuery';
 
 function Test() {
   const user = useRecoilValue(authState);
-  const userDoc = useUserDoc();
+  // const userDoc = useUserDoc();
+  const userDoc = useUserQuery();
+  console.log("🚀 ~ file: Test.jsx:10 ~ Test ~ userDoc:", userDoc)
+
+  if (userDoc?.isLoading) {
+    return <div>로딩 중</div>;
+  }
+  if (userDoc?.isError) {
+    return <div>에러</div>;
+  }
 
   return (
     <div>
@@ -13,10 +23,12 @@ function Test() {
       <p>uid : {user?.uid}</p>
       <p>displayName : {user?.displayName}</p>
       <p>photoURL : {user?.photoURL}</p>
-      <p>email : {userDoc?.email}</p>
       <br />
-      <h1>post 컬렉션 TEST</h1>
+      <h1>user 컬렉션 TEST</h1>
       <br />
+      <p>{userDoc?.email}</p>
+      <p>{userDoc?.moreInfo.u_stack}</p>
+      <p>{userDoc?.teamID}</p>
     </div>
   );
 }
