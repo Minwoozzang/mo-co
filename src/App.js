4ed24440523df8c89c104651fee4051b7008c0f5
2sel <yunwel0610@gmail.com>
2023-03-07 09:37:31 +0900

    feat. commentState 추가

--- src/App.js
+++ src/App.js
@@ -2,30 +2,19 @@ import { useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { authService } from './common/firebase';
 import GlobalStyle from './components/GlobalStyle';
-<<<<<<< HEAD
+import useTeamPage from './hooks/useTeamPage';
 import authState from './recoil/authState';
 import commentState from './recoil/commentState';
 import Router from './shared/router';
 import useComment from './hooks/useComment';
-
-function App() {
-  const setAuthState = useSetRecoilState(authState);
-  const setCommentState = useSetRecoilState(commentState);
-  const comment = useComment();
-
-  console.log(comment);
-=======
-import useTeamPage from './hooks/useTeamPage';
-import authState from './recoil/authState';
-import Router from './shared/router';
 import teamPageState from './recoil/teamPageState';
 
 function App() {
   const setAuthState = useSetRecoilState(authState);
-
+  const setCommentState = useSetRecoilState(commentState);
   const setTeamPageState = useSetRecoilState(teamPageState);
   const teamPage = useTeamPage();
->>>>>>> b6b3c516401226199be752194a774f0cbdfe2b6b
+  const comment = useComment();
 
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
@@ -40,13 +29,9 @@ function App() {
         return;
       }
     });
-<<<<<<< HEAD
-  }, [authService.currentUser]);
-=======
 
     setTeamPageState(teamPage.data);
   }, [authService.currentUser, teamPage]);
->>>>>>> b6b3c516401226199be752194a774f0cbdfe2b6b
 
   return (
     <>
