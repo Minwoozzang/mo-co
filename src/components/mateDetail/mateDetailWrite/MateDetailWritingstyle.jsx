d9a5dd2f3b95d5b149136e87da0b62e45945146f
2sel <yunwel0610@gmail.com>
2023-03-09 19:03:45 +0900

    fix. 반응형  css 수정

--- src/components/mateDetail/mateDetailWrite/MateDetailWritingstyle.jsx
+++ src/components/mateDetail/mateDetailWrite/MateDetailWritingstyle.jsx
@@ -15,7 +15,7 @@ export const GroupHeader = styled.h4`
 
   /* 아이패드 프로 */
   @media only screen and (min-device-width: 1024px) and (max-device-width: 1366px) {
-    padding-left: 11rem;
+    padding-left: 9.5rem;
   }
 
   /* 태블릿, 아이패드 */
@@ -33,6 +33,11 @@ export const UserInfoWrap = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
+
+  /* 아이패드 프로 */
+  @media only screen and (min-device-width: 1024px) and (max-device-width: 1366px) {
+    margin-right: 8rem;
+  }
 `;
 
 export const GroupImg = styled.img`
@@ -44,7 +49,6 @@ export const GroupImg = styled.img`
 export const GroupUserId = styled.p`
   width: 10%;
   margin: 2.6875rem 0.5rem 0.5rem;
-
   color: #fff;
 `;
 
@@ -52,6 +56,10 @@ export const Social = styled.div`
   padding-left: 25%;
   width: 50%;
   display: flex;
+  /* 아이패드 프로 */
+  @media only screen and (min-device-width: 1024px) and (max-device-width: 1366px) {
+    padding-left: 28%;
+  }
 `;
 
 export const DropdownOptions = styled(FiShare2)`
@@ -69,8 +77,14 @@ export const GroupBox = styled.div`
   background-color: #232323;
   border-radius: 1.25rem;
   color: #fff;
-  /* 태블릿, 아이패드 */
 
+  /* 아이패드 프로 */
+  @media only screen and (min-device-width: 1024px) and (max-device-width: 1366px) {
+    max-width: 500px;
+    margin: 0 0 0 150px;
+  }
+
+  /* 태블릿, 아이패드 */
   @media only screen and (min-device-width: 768px) and (max-device-width: 1023px) {
     max-width: 550px;
     margin: 0 0 0 140px;
