3eb2da1f1fcfd6c253eaba1c47eccdff309962d4
coachkim <kimsw0825@gmail.com>
2023-03-10 14:12:16 +0900

    fix: 커스텀리스트 모임 없을 때 디자인 수정

--- src/components/home/meeting/TimeMeeting.jsx
+++ src/components/home/meeting/TimeMeeting.jsx
@@ -75,9 +75,9 @@ const TimeMeeting = ({ currentUserData }) => {
               <TimeMeetingCardBox>
                 {recommendTimeList.length === 0 ? (
                   <NonRecommendText2>
-                    추천 모임이 없습니다.
+                    ⚠️ 추천 모임이 없습니다 ⚠️
                     <br />
-                    추가 정보를 등록 or 수정해주세요!
+                    세부 정보를 등록하거나 모임을 만들어 보세요
                   </NonRecommendText2>
                 ) : (
                   recommendTimeList
@@ -148,8 +148,7 @@ const TimeMeeting = ({ currentUserData }) => {
             )}
           </TimeMeetingMediaCardBox>
         </TimeMeetingMediaBox>
-      )
-      }
+      )}
     </TimeMeetingArea>
   );
 };
