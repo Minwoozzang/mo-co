7771bbb3c4b3eda8f603d90247aa8db67bf27975
wallofwolf <77008335+wallofwolf@users.noreply.github.com>
2023-03-11 11:06:55 +0900

    chore : 테스트용 firebase api키로 교체

--- src/common/firebase.js
+++ src/common/firebase.js
@@ -5,20 +5,6 @@ import { getStorage } from 'firebase/storage';
 import 'firebase/database';
 
 const firebaseConfig = {
-  // apiKey: 'AIzaSyD21UrEfLRzoDK3l5BY3NLurGrp2noWeUU',
-  // authDomain: 'moco-practice.firebaseapp.com',
-  // projectId: 'moco-practice',
-  // storageBucket: 'moco-practice.appspot.com',
-  // messagingSenderId: '959088055425',
-  // appId: '1:959088055425:web:49e6065793e16a2ae66980',
-
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
