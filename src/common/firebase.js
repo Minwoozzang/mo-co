1c708724cc2db0696b12a6e17ac6d4e52fa1a407
coachkim <kimsw0825@gmail.com>
2023-03-13 09:10:46 +0900

    pull받기 전 커밋

--- src/common/firebase.js
+++ src/common/firebase.js
@@ -5,22 +5,13 @@ import { getStorage } from 'firebase/storage';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCzN2tCMYsWXAox6D_olgaMVQM_DD5X0Tc',
-  authDomain: 'moco-project-defe9.firebaseapp.com',
-  databaseURL:
-    'https://moco-project-defe9-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'moco-project-defe9',
-  storageBucket: 'moco-project-defe9.appspot.com',
-  messagingSenderId: '854864723646',
-  appId: '1:854864723646:web:c0f50ad676b017739ea8ef',
-
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
